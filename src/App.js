a532ebb1e8094979f887839d7fa397880ae8e68a
root <manishakosgi00@gmail.com>
2023-12-30 13:14:41 +0000

    app store

---
+++ src/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/extensions */
+/* eslint-disable spaced-comment */
+import AppStore from './components/AppStore/index.js'
+
+import './App.css'
+
+const App = () => <AppStore />
+
+export default App
